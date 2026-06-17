b8076085f8e2d68ccb226d5078adfa2eac910188
DHADHAYA <145863092+DHADHAYA@users.noreply.github.com>
2026-06-17 15:35:36 +0200

    Added basic javascript

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function generatePoem(event){
+    event.preventDefault();
+
+    let poemElement = document.querySelector("poem");
+
+new Typewriter("#poem", {
+  strings:  "La tombe dit a la rose",
+  autoStart: true,
+  delay:1, 
+  cursor:"",
+});
+
+
+
+    poemElement.innerHTML = "La tombe dit a la rose";
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit" ,generatePoem);
